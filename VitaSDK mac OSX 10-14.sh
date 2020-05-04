79691c8196dbda82638a2101e802660498e24dd9
modz2014 <dawgz1992.jp@gmail.com>
2020-05-05 01:58:08 +0800

    making things easier for Mac users

---
+++ VitaSDK mac OSX 10-14.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+
+var=" #### Here we are going to edit your .bashrc file ####"
+printf "%s\n" "$var"
+
+cp /etc/bashrc /Users/modz2014/Desktop/bashrc.bak
+
+echo 'export VITASDK=/usr/local/vitasdk' >> /etc/bashrc
+echo 'export PATH=$VITASDK/bin:$PATH' >> /etc/bashrc
+
+var1=" #### Downloading VITASDK OSX ####"
+printf "%s\n" "$var1"
+
+wget https://github.com/vitasdk/autobuilds/releases/download/master-osx-v1101/vitasdk-x86_64-apple-darwin-2020-05-01_18-23-01.tar.bz2
+
+var2=" #### Extracting VIATSDK ####"
+printf "%s\n" "$var2"
+
+tar -xvf vitasdk-x86_64-apple-darwin-2020-05-01_18-23-01.tar.bz2 -C /usr/local/
+
+echo " ### DONE ###"
+
+file="vitasdk-x86_64-apple-darwin-2020-05-01_18-23-01.tar.bz2"
+
+if [ -f $file ] ; then
+    rm $file
+fi
+
+file="wget-log"
+
+if [ -f $file ] ; then
+  rm $file
+fi
